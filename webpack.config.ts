ddba63c6433697462881a3e52eb634bcc345da85
Jay Pitman <jakeseanp@gmail.com>
2020-02-10 22:11:01 +1100

    Add HtmlWebpackPlugin & CleanWebpackPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,24 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import path from "path";
 import webpack from "webpack";
 
 const config: webpack.Configuration = {
   mode: 'development',
-  entry: './src/index.tsx',
+  entry: {
+    main: './src/index.tsx'
+  },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js', '.json' ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: '[name].bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin()
+  ],
   module: {
     rules: [
       {
