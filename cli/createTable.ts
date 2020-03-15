cd2c20cca210ff3b80a318b7e07d5b26e2ab9fba
Jay Pitman <jakeseanp@gmail.com>
2020-03-15 13:37:14 +1100

    Add variable assignments column

--- cli/createTable.ts
+++ cli/createTable.ts
@@ -11,17 +11,23 @@ export default (
       value: char
     }
   })
+  header.unshift({ value: "Variable Assignments" } )
   const rows = evaluatedRows.map((evaluation) => {
     const { evaluatedRow, workingRow } = evaluation.rowObject
     const overallTruthValue = workingRow.find(e => {return typeof e === "number"})
     const indexOfOverallTruthValue = workingRow.indexOf(overallTruthValue)
-    return evaluatedRow.map((char, i) => {
+    const result = evaluatedRow.map((char, i) => {
       if (i === indexOfOverallTruthValue) {
         return char === 1 ? chalk.green("1") : chalk.red("0")
       } else {
         return char === null ? "" : `${char}`
       }
     })
+    const variableAssignmentsString = Object.keys(evaluation.variableAssignments).map(variableKey => {
+      return `${variableKey}: ${evaluation.variableAssignments[variableKey]}`
+    }).join(", ")
+    result.unshift(variableAssignmentsString)
+    return result
   })
 
   return Table(header, rows).render()
