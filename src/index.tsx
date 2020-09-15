2bcad026f4e8a1db6f43936e324ecfa8f8df04c0
Jake Pitman <jakeseanp@gmail.com>
2020-09-15 16:48:41 +1000

    Render App in root, not document.body

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.body
-)
+const root = document.createElement("div");
+root.setAttribute("id", "root");
+document.body.appendChild(root);
+
+ReactDOM.render(<App />, root);
